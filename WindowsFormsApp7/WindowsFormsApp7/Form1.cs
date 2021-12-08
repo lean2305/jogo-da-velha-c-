e18aab41702ab3634b2311b31d450d5a035c3631
lean2305 <74493494+lean2305@users.noreply.github.com>
2021-12-08 17:04:02 +0000

    Add files via upload

---
+++ WindowsFormsApp7/WindowsFormsApp7/Form1.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp7
+{
+    public partial class Form1 : Form
+    {
+        int a=0,b=0,c=0;
+        int d=0, e=0, f=0;
+        int x=0, o=1;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (x < o)
+            {
+
+                button2.Text = "X";
+                x = x + 1;
+
+            }//
+
+            //
+            else if (o == x)
+            {
+
+                button2.Text = "O";
+                o = o + 1;
+
+            }//
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (x < o)
+            {
+
+                button3.Text = "X";
+                x = x + 1;
+
+            }//
+
+            //
+            else if (o == x)
+            {
+
+                button3.Text = "O";
+                o = o + 1;
+
+            }//
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (x < o)
+            {
+
+                button4.Text = "X";
+                x = x + 1;
+
+            }//
+
+            //
+            else if (o == x)
+            {
+
+                button4.Text = "O";
+                o = o + 1;
+
+            }//
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (x < o)
+            {
+
+                button5.Text = "X";
+                x = x + 1;
+
+            }//
+
+            //
+            else if (o == x)
+            {
+
+                button5.Text = "O";
+                o = o + 1;
+
+            }//
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (x < o)
+            {
+
+                button6.Text = "X";
+                x = x + 1;
+
+            }//
+
+            //
+            else if (o == x)
+            {
+
+                button6.Text = "O";
+                o = o + 1;
+
+            }//
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (x < o)
+            {
+
+                button7.Text = "X";
+                x = x + 1;
+
+            }//
+
+            //
+            else if (o == x)
+            {
+
+                button7.Text = "O";
+                o = o + 1;
+
+            }//
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (x < o)
+            {
+
+                button8.Text = "X";
+                x = x + 1;
+
+            }//
+
+            //
+            else if (o == x)
+            {
+
+                button8.Text = "O";
+                o = o + 1;
+
+            }//
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (x < o)
+            {
+
+                button9.Text = "X";
+                x = x + 1;
+
+            }//
+
+            //
+            else if (o == x)
+            {
+
+                button9.Text = "O";
+                o = o + 1;
+
+            }//
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //
+            if (x < o) {
+            
+                button1.Text = "X";
+                    x=x+1;
+            
+            }//
+
+            //
+            else if (o == x)
+            {
+                
+                    button1.Text = "O";
+                    o = o + 1;
+                
+            }//
+
+
+        }
+
+        int g =0, h=0, i=0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+       
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+
+         
+
+        }
+    }
+}
